4e54842d7bb4837828d1bcdb844f9e40fcca05a0
Noelia <noeliarico@uniovi.es>
2020-03-03 10:46:34 +0100

    dists package

---
+++ data-raw/toy_data.R
@@ -0,0 +1,15 @@
+toy_data <- clusterlab::clusterlab(centers = 3,
+                                   r = 2,
+                                   numbervec = c(5, 5, 5),
+                                   sdvec = sample(1:3, 3),
+                                   alphas = sample(1:3, 3),
+                                   features = 2,
+                                   seed = 2020)
+toy_data <- as_tibble(t(toy_data$synthetic_data)) %>%
+  mutate(cluster = as.factor(toy_data$identity_matrix$cluster)) %>%
+  mutate_if(is.numeric, normalize)
+
+ggplot(toy_data, aes(V1, V2, color = cluster)) +
+  geom_point()
+
+usethis::use_data(toy_data, overwrite = TRUE)
