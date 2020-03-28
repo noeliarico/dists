7728becedb3f98fdcc17fc25be249215702b244c
noeliarico <noeliarico@uniovi.es>
2020-03-28 19:42:21 +0100

    Comment add_anotations in plot

--- R/plot.R
+++ R/plot.R
@@ -15,9 +15,9 @@ plotDistance <- function(index1, index2, df, distance) {
   y1 = df[index1, 2]
   y2 = df[index2, 2]
 
-  switch (distance,
-          "manhattan" = add_annotation_manhattan(x1, x2, y1, y2),
-          "euclidean" = add_annotation_euclidean(x1, x2, y1, y2),
-          "chebyshev" = add_annotation_chebyshev(x1, x2, y1, y2)
-  )
+  # switch (distance,
+  #         "manhattan" = add_annotation_manhattan(x1, x2, y1, y2),
+  #         "euclidean" = add_annotation_euclidean(x1, x2, y1, y2),
+  #         "chebyshev" = add_annotation_chebyshev(x1, x2, y1, y2)
+  # )
 }
