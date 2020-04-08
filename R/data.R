5559c88bb8e81165be5bd49048f91f328377ed55
noeliarico <noeliarico@uniovi.es>
2020-04-08 18:01:53 +0200

    Document datasets

---
+++ R/data.R
@@ -0,0 +1,39 @@
+#' Toy dataset
+#'
+#' @format A tibble with 15 objects and 2 features + real cluster:
+#' \describe{
+#'   \item{V1}{Numeric feature}
+#'   \item{V2}{Numeric feature}
+#'   \item{V2}{Cluster of the object}
+#' }
+"toy_data"
+
+#' Toy dataset
+#'
+#' @format A tibble with 5 objects and 2 features:
+#' \describe{
+#'   \item{V1}{Numeric feature}
+#'   \item{V2}{Numeric feature}
+#' }
+"toy_data1"
+
+#' Toy dataset
+#'
+#' @format A tibble with 5 objects and 3 features:
+#' \describe{
+#'   \item{V1}{Numeric feature}
+#'   \item{V2}{Numeric feature}
+#'   \item{V3}{Numeric feature}
+#' }
+"toy_data2"
+
+#' Toy dataset
+#'
+#' @format A tibble with 5 objects and 4 features:
+#' \describe{
+#'   \item{V1}{Numeric feature}
+#'   \item{V2}{Numeric feature}
+#'   \item{V3}{Numeric feature}
+#'   \item{V4}{Numeric feature}
+#' }
+"toy_data3"
